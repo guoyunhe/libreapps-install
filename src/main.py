953386acf690cd006487e2fbe443d71925f2db8e
Guo Yunhe <guoyunhebrave@gmail.com>
2015-10-18 21:06:51 +0300

    Change exception output.

--- src/main.py
+++ src/main.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     try:
         bus = dbus.SessionBus()
     except dbus.DBusException, e:
-        print 'Unable to connect to dbus: %s' % str(e)
+        print 'PackageKit Connection Expception: %s' % str(e)
         sys.exit()
     try:
         proxy = bus.get_object('org.freedesktop.PackageKit', '/org/freedesktop/PackageKit')
         iface = dbus.Interface(proxy, 'org.freedesktop.PackageKit.Modify')
         iface.InstallPackageNames(dbus.UInt32(0), packages, "show-confirm-search,hide-finished")
     except dbus.DBusException, e:
-        print 'Unable to use PackageKit: %s' % str(e)
+        print 'PackageKit Exception: %s' % str(e)
