8d1cb597aa1fa95cca372834315bf86241a6968c
Guo Yunhe <guoyunhebrave@gmail.com>
2015-10-18 21:04:25 +0300

    Handle a URL argument and install package.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
-
 __author__ = "Guo Yunhe <guoyunhebrave@gmail.com>"
 __date__ = "$2015-10-17 14:36:39$"
 
 import dbus
+import argparse
 
 if __name__ == "__main__":
-    print "Hello World"
-
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('url', type=str, help='URL of one click package installation')
+    args = parser.parse_args()
+    url = args.url
+    packages = url[8:].split(',')
+    
     try:
         bus = dbus.SessionBus()
     except dbus.DBusException, e:
@@ -15,6 +19,6 @@ if __name__ == "__main__":
     try:
         proxy = bus.get_object('org.freedesktop.PackageKit', '/org/freedesktop/PackageKit')
         iface = dbus.Interface(proxy, 'org.freedesktop.PackageKit.Modify')
-        iface.InstallPackageNames(dbus.UInt32(0), ["openoffice-clipart", "openoffice-clipart-extras"], "show-confirm-search,hide-finished")
+        iface.InstallPackageNames(dbus.UInt32(0), packages, "show-confirm-search,hide-finished")
     except dbus.DBusException, e:
         print 'Unable to use PackageKit: %s' % str(e)
