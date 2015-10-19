e11afeab1d91f2f1fa5d6ceb466ce0ec5f1d485e
Guo Yunhe <guoyunhebrave@gmail.com>
2015-10-19 13:02:25 +0300

    Support GNU/Linux distros and versions pattern.

--- src/libreapps.py
+++ src/libreapps.py
@@ -5,15 +5,35 @@ __date__ = "$2015-10-17 14:36:39$"
 
 import dbus
 import argparse
+import platform
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('url', type=str, help='URL of one click package installation')
     args = parser.parse_args()
     url = args.url
-    # TODO parse packages and check distro type
-    packages = url[8:].split(',')
     
+    # check distro info
+    (distname, version, id) = platform.linux_distribution()
+    
+    # parse packages
+    patterns = url[8:].split('&')
+    
+    ## general packages
+    packages = patterns[0].split(',')
+    
+    ## distro-specified packages
+    distname_prefix = distname.strip().lower() + '='
+    for pattern in patterns:
+        if pattern.find(distname_prefix) == 0:
+            packages = pattern.replace(distname_prefix, '').split(',')
+    
+    ## distro-version-specified packages
+    distname_version_prefix = distname.strip().lower() + ':' + version.strip().lower() + '='
+    for pattern in patterns:
+        if distname_version_prefix in pattern:
+            packages = pattern.replace(distname_version_prefix, '').split(',')
+        
     try:
         bus = dbus.SessionBus()
     except dbus.DBusException, e:
